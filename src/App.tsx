7e2c790aab6de9268b8f43e5e8cc8c2c424f0adb
KayCodes11 <kayydraws@gmail.com>
2022-08-07 21:58:14 +0530

    react router v6 practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Post from "./pages/Post";
+import PostLayout from "./pages/PostLayout";
+import PostDetail from "./pages/PostDetail";
+import Layout from "./pages/Layout";
+import "./App.css";
 
-// make branch from this branch
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="about" element={<About/>}/>
+                    <Route path="contact" element={<Contact/>}/>
+                    <Route path="post" element={<PostLayout/>}>
+                        <Route path=":category" element={<PostDetail/>}/>
+                        <Route index element={<Post/>}/>
+                    </Route>
+                    <Route index element={<Home/>}/>
+                </Route>
+                <Route path="*" element={<h1>Error 404 Page not Found !!</h1>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
