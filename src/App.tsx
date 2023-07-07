40f45ad6f7ac8360c7dc3aa7fc01b20e6a1db1d9
Kaycodes11 <kayydraws@gmail.com>
2023-07-07 19:19:44 +0530

    react pdf for resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,19 @@
-import React from "react";
-import "./App.css";
+import { Text, Font, Page, View, Image, Document, StyleSheet } from "@react-pdf/renderer";
+import Resume from "./components/Resume";
 
-type User = { name: string; id: string };
-type LoadingStatus = { loading: "STARTED" | "DONE" };
-
-function doInterval() {
-  console.log("do something");
-}
-
-// React + TypeScript usage guide: https://react-typescript-cheatsheet.netlify.app
-// https://fettblog.eu/typescript-react-extending-jsx-elements/
-// https://stackoverflow.com/questions/64867112/difference-between-react-htmlpropshtmldivelement-and-react-detailedhtmlpropsr
-// https://www.saltycrane.com/cheat-sheets/typescript/react/latest/
-// https://stackoverflow.com/questions/66348283/to-extend-the-react-select-interface-property-in-typescript?rq=4
-function App() {
-  const [user, setUser] = React.useState<User | null>(null);
-  const [status, setStatus] = React.useState<LoadingStatus>({} as LoadingStatus);
-  const intervalRef = React.useRef<number | null>(null);
-  // it's standard to initialize `useRef` with null, but tell TypeScript we're looking for HTMLInputElement
-  const inputEl = React.useRef<HTMLInputElement>(null);
-
-  React.useEffect(() => {
-    if (intervalRef.current) {
-      intervalRef.current = window.setInterval(doInterval, 2500);
-    }
-    return () => {
-      clearInterval(intervalRef.current!);
-    };
-  }, []);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    if (inputEl && inputEl.current) inputEl.current.focus();
-  };
-
-  const handleInput = (event: React.ChangeEventHandler<HTMLInputElement>) => {};
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
-
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div style={{ display: "flex", gap: "0.2rem" }}>
-          <input ref={inputEl} type="text" />
-          <button onClick={handleClick}>focus input</button>
-        </div>
-      </header>
+      <Document
+        author="Pallab Kayal"
+        keywords="Full Stack developer +2.5 years experience"
+        subject="The resume of Pallab Kayal"
+        title="Resume"
+      >
+        <Resume size="A4" />
+        <Resume orientation="landscape" size="A4" />
+        <Resume size={[380, 1250]} />
+      </Document>
     </div>
   );
 }
-
-export default App;
