2187b8ff00bef9d8aadf33e45d814e355a5029d2
Kaycodes11 <kayydraws@gmail.com>
2023-07-26 03:40:59 +0530

    multi language with i18n

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Header from "./components/Navbar/Header";
+
+// https://github.dev/banishnarang/yt-react-i18next
+function App() {
+  return (
+    <React.Suspense fallback={null}>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+    </React.Suspense>
+  );
+}
+
+export default App;
